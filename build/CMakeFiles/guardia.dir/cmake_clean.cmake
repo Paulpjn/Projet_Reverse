0ab8007638ba27a38f256f972cd42696892ca16f
frenzyfarm <blaur@guardiaschool.fr>
2025-04-08 17:48:30 +0200

    Add files via upload

---
+++ build/CMakeFiles/guardia.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guardia.dir/main.c.o"
+  "CMakeFiles/guardia.dir/main.c.o.d"
+  "guardia"
+  "guardia.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/guardia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/guardia.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for guardia.
+# This may be replaced when dependencies are built.
